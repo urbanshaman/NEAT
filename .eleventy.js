1285d36c9186f1238e82369aa052b27255e5545f
Urban Shaman <32364484+urbanshaman@users.noreply.github.com>
2022-02-03 15:45:38 +0300

    Master (#1)
    * Upgraded eleventy to fix vulnerability
    
    Upgraded eleventy to 0.12.1 to fix high severity vulnerability
    
    * call browsersync explictly for simpler watching (#10)
    
    * call browsersync explictly for simpler watching
    
    * change Browsersync CLI to current 11ty defaults
    
    * Update README.md
    
    * Remove duplicated .gitignore entry
    
    * Updated to alpine.js version 3.
    
    * update dependencies
    
    * fix error with yaml.safeLoad
    
    * update tailwind.config
    
    * Fix Alpine.js click away issue
    
    * change to tailwindcli
    
    * update gitignore
    
    Co-authored-by: Surjith S M <surjithctly@gmail.com>
    Co-authored-by: Brady <47389606+bradybellini@users.noreply.github.com>
    Co-authored-by: P. Edmon Graham <homosaur@gmail.com>
    Co-authored-by: Codrin Pavel <zerospree@yahoo.com>
    Co-authored-by: Rodrigo Escandón Cesarman <rodrigo.escandon.cesarman@gmail.com>
    Co-authored-by: Luis Steidle <luis@steidle.me>

--- .eleventy.js
+++ .eleventy.js
@@ -22,18 +22,12 @@ module.exports = function (eleventyConfig) {
 
   // To Support .yaml Extension in _data
   // You may remove this if you can use JSON
-  eleventyConfig.addDataExtension("yaml", (contents) =>
-    yaml.safeLoad(contents)
-  );
-
-  // Add Tailwind Output CSS as Watch Target
-  eleventyConfig.addWatchTarget("./_tmp/static/css/style.css");
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
-    "./_tmp/static/css/style.css": "./static/css/style.css",
     "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
+    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
     "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
   });
@@ -51,7 +45,7 @@ module.exports = function (eleventyConfig) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
       });
       return minified;
     }
